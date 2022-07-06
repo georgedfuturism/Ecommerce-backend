88155b325ab2e93fc87a770c23b02fc349810b60
georgedfuturism <107398778+georgedfuturism@users.noreply.github.com>
2022-07-06 22:37:19 +0530

    added api into server

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Nike Slim shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
@@ -13,6 +14,8 @@ const data = {
       description: 'high quality shirt',
     },
     {
+      _id: '2',
+
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -25,6 +28,8 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '3',
+
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -37,6 +42,8 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '4',
+
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
@@ -49,6 +56,8 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '5',
+
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
@@ -61,6 +70,8 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '6',
+
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -73,18 +84,22 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '7',
+
       name: 'Nike Slim shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
       image: '/images/p1.jpg', // 679px × 829px
       price: 120,
-      countInStock: 0,
+      countInStock: 2,
       brand: 'Nike',
       rating: 4.5,
       numReviews: 10,
       description: 'high quality shirt',
     },
     {
+      _id: '8',
+
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
