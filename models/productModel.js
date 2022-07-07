28698bf2987774f58b62595dc634305e6f2ed95d
georgedfuturism <107398778+georgedfuturism@users.noreply.github.com>
2022-07-07 16:16:56 +0530

    create sample productsb

--- models/productModel.js
+++ models/productModel.js
@@ -1,16 +1,5 @@
 import mongoose from 'mongoose';
 
-// const reviewSchema = new mongoose.Schema(
-//   {
-//     name: { type: String, required: true },
-//     comment: { type: String, required: true },
-//     rating: { type: Number, required: true },
-//   },
-//   {
-//     timestamps: true,
-//   }
-// );
-
 const productSchema = new mongoose.Schema(
   {
     name: { type: String, required: true, unique: true },
