d80448f3f61de08aea2fce1164f54e2b3ebd2927
georgedfuturism <107398778+georgedfuturism@users.noreply.github.com>
2022-07-07 16:34:45 +0530

    create sample usresb

--- data.js
+++ data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs';
+
 const data = {
+  users: [
+    {
+      name: 'George',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: true,
+    },
+    {
+      name: 'John',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('123456'),
+      isAdmin: false,
+    },
+  ],
   products: [
     {
       // _id: '1',
@@ -7,7 +23,7 @@ const data = {
       category: 'Shirts',
       image: '/images/p1.jpg', // 679px × 829px
       price: 120,
-      countInStock: 0,
+      countInStock: 10,
       brand: 'Nike',
       rating: 4.5,
       numReviews: 10,
@@ -15,13 +31,12 @@ const data = {
     },
     {
       // _id: '2',
-
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
       image: '/images/p2.jpg',
       price: 250,
-      countInStock: 20,
+      countInStock: 0,
       brand: 'Adidas',
       rating: 4.0,
       numReviews: 10,
@@ -29,7 +44,6 @@ const data = {
     },
     {
       // _id: '3',
-
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -43,63 +57,6 @@ const data = {
     },
     {
       // _id: '4',
-
-      name: 'Adidas Fit Pant',
-      slug: 'adidas-fit-pant',
-      category: 'Pants',
-      image: '/images/p4.jpg',
-      price: 65,
-      countInStock: 5,
-      brand: 'Puma',
-      rating: 4.5,
-      numReviews: 10,
-      description: 'high quality product',
-    },
-    {
-      // _id: '5',
-
-      name: 'Adidas Fit Pant',
-      slug: 'adidas-fit-pant',
-      category: 'Pants',
-      image: '/images/p4.jpg',
-      price: 65,
-      countInStock: 5,
-      brand: 'Puma',
-      rating: 4.5,
-      numReviews: 10,
-      description: 'high quality product',
-    },
-    {
-      // _id: '6',
-
-      name: 'Adidas Fit Shirt',
-      slug: 'adidas-fit-shirt',
-      category: 'Shirts',
-      image: '/images/p2.jpg',
-      price: 250,
-      countInStock: 20,
-      brand: 'Adidas',
-      rating: 4.0,
-      numReviews: 10,
-      description: 'high quality product',
-    },
-    {
-      // _id: '7',
-
-      name: 'Nike Slim shirt',
-      slug: 'nike-slim-shirt',
-      category: 'Shirts',
-      image: '/images/p1.jpg', // 679px × 829px
-      price: 120,
-      countInStock: 2,
-      brand: 'Nike',
-      rating: 4.5,
-      numReviews: 10,
-      description: 'high quality shirt',
-    },
-    {
-      // _id: '8',
-
       name: 'Adidas Fit Pant',
       slug: 'adidas-fit-pant',
       category: 'Pants',
